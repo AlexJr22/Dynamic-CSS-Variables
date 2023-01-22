32909739cfe1a2e6730d623bb120a4811caa75c5
Alex Junior <alexjr2oficial@gmail.com>
2023-01-22 13:13:29 -0300

    criando funções que escutÃam os botões de movimento

--- js/script.js
+++ js/script.js
@@ -9,4 +9,25 @@ const selectSquare = (index) => {
   elements[index].classList.add("active");
 };
 
+// controles de movimento
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+const btnUp = document.getElementById("btnUp");
+const btnDown = document.getElementById("btnDown");
+
 // escutar os controles
+btnLeft.addEventListener("click", () => {
+  console.log("esqueda");
+});
+
+btnRight.addEventListener("click", () => {
+  console.log("direita");
+});
+
+btnUp.addEventListener("click", () => {
+  console.log("cima");
+});
+
+btnDown.addEventListener("click", () => {
+  console.log("baixo");
+});
