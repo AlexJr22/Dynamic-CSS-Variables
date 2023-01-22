32dc09d57e3db47a1304f5b7d2d2877e1d9d8584
Alex Junior <alexjr2oficial@gmail.com>
2023-01-22 14:32:14 -0300

    criando uma função que movimenta o quadrado

--- js/script.js
+++ js/script.js
@@ -4,20 +4,33 @@ const btnRight = document.getElementById("btnRight");
 const btnUp = document.getElementById("btnUp");
 const btnDown = document.getElementById("btnDown");
 
+const square = document.getElementById("square");
+
 // escutar os controles
+
+let left = 0;
+let Top = 0;
+
 btnLeft.addEventListener("click", () => {
-  console.log("esqueda");
-  
+  console.log("left");
+
+  square.style.setProperty("left", `${(left -= 10)}px`);
 });
 
 btnRight.addEventListener("click", () => {
-  console.log("direita");
+  console.log("right");
+
+  square.style.setProperty("left", `${(left += 10)}px`);
 });
 
 btnUp.addEventListener("click", () => {
-  console.log("cima");
+  console.log("up");
+
+  square.style.setProperty("top", `${(Top -= 10)}px`);
 });
 
 btnDown.addEventListener("click", () => {
-  console.log("baixo");
+  console.log("down");
+  
+  square.style.setProperty("top", `${(Top += 10)}px`);
 });
